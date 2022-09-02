7d0928f573cfffea9af7f8413af7abc02e18081d
lillythamolwan <thamolwan.thi@g.swu.ac.th>
2022-09-02 10:49:44 +0000

    insert data

--- 02-data-modeling-ii/etl.py
+++ 02-data-modeling-ii/etl.py
@@ -1,14 +1,22 @@
+import glob
+import json
+import os
+from typing import List
+
 from cassandra.cluster import Cluster
 
-
+#Drop table ใช้สำหรับล้างข้อมูลตารางเดิมเพื่อรันในครั้งต่อไป
 table_drop = "DROP TABLE events"
 
+#สร้างตัวแปรเก็บข้อมูลในตารางด้วยภาษา sql
 table_create = """
     CREATE TABLE IF NOT EXISTS events
     (
         id text,
         type text,
         public boolean,
+        created_at text
+        
         PRIMARY KEY (
             id,
             type
@@ -38,6 +46,23 @@ def create_tables(session):
         except Exception as e:
             print(e)
 
+#Fuction ดึงข้อมูลจากไฟล์ .json 
+def get_files(filepath: str) -> List[str]:
+    """
+    Description: This function is responsible for listing the files in a directory
+    """
+
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root, "*.json"))
+        for f in files:
+            all_files.append(os.path.abspath(f))
+
+    num_files = len(all_files)
+    print(f"{num_files} files found in {filepath}")
+
+    return all_files
+
 
 def process(session, filepath):
     # Get list of files from filepath
@@ -48,14 +73,18 @@ def process(session, filepath):
             data = json.loads(f.read())
             for each in data:
                 # Print some sample data
-                print(each["id"], each["type"], each["actor"]["login"])
+                print(each["id"], each["type"], each["public"], each["created_at"])
 
                 # Insert data into tables here
+                query = f"""
+                INSERT INTO events (id, type, public, created_at) VALUES ('{each["id"]}', '{each["type"]}', {each["public"]}, '{each["created_at"]}')
+                """
+                session.execute(query)
 
 
 def insert_sample_data(session):
     query = f"""
-    INSERT INTO events (id, type, public) VALUES ('23487929637', 'IssueCommentEvent', true)
+    INSERT INTO events (id, type, public, created_at) VALUES ('23487929661', 'PushEvent', true, '2022-08-17T15:51:05Z')
     """
     session.execute(query)
 
@@ -84,12 +113,15 @@ def main():
     drop_tables(session)
     create_tables(session)
 
-    # process(session, filepath="../data")
-    insert_sample_data(session)
+    process(session, filepath="../data")
+    # insert_sample_data(session)
 
     # Select data in Cassandra and print them to stdout
+    #query = """
+    #SELECT * from events WHERE id = '23487929661' AND type = 'PushEvent'
+    #"""
     query = """
-    SELECT * from events WHERE id = '23487929637' AND type = 'IssueCommentEvent'
+    SELECT * from events WHERE type = 'PushEvent' #ALLOW FILTERING
     """
     try:
         rows = session.execute(query)
