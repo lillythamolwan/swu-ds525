b74e15aac004e4508f5b56a4053e5519627c50dd
lillythamolwan <thamolwan.thi@g.swu.ac.th>
2022-12-13 17:18:11 +0000

    upload file s3

--- 00-capstone-project/main.py
+++ 00-capstone-project/main.py
@@ -1,8 +1,9 @@
 import boto3
 
-aws_access_key_id = "ASIAYXMWM46LPU36XUN5"
-aws_secret_access_key = "JQaUhLh2Qqgiwyw1yPBo0I2UcdbDtvj1eXQUdYcP"
-aws_session_token = "FwoGZXIvYXdzEJL//////////wEaDKhZFxxiZkD5Nc4bMSLKAWZ/6f1IgKPenyfWUHNNzAPPt9DDVo/uKHx+1LNTi01obT3nnqMwk6I15G96vGHxMXSpak5edS/T/OCXtyzBPVctZsQ7LtKCGeFWSfB/mN7Qhh1XyvRLzPOtJRhcH98Y5bzYUrF1snB38XyBCB5ViWXPkTvVZIKCqSW5CzgDILELGZZE6cByO1INHKe8OJl2GWA4StXmBpORQi3U8wSa221/KKKaQSMn3r6Hd/LS+6aRuSOwYZwgyT0sg4pNqVhJaUI5ET4CvWy/ltIomqfdnAYyLcv7EuN79RAlqykrhUkAnFxPTN+zxDB17jB7C5dv14ZqWZvGUK9QtbN26ELRIg=="
+aws_access_key_id = ""
+aws_secret_access_key = ""
+aws_session_token = ""
+
 
 #client = boto3.client(
 #    "s3",
@@ -19,7 +20,13 @@ s3 = boto3.resource(
     aws_session_token=aws_session_token
 )
 s3.meta.client.upload_file(
-    "Count of BASKET_ID by Department.csv",
+    "transaction_data.csv",
     "lillythamolwan-titanic",
-    "Count of BASKET_ID by Department.csv",
+    "transaction_data.csv",
+)
+
+s3.meta.client.upload_file(
+    "product.csv",
+    "lillythamolwan-titanic",
+    "product.csv",
 )
