2e16e06b2f430498773c2da9f2a04bf8c9e118df
lillythamolwan <thamolwan.thi@g.swu.ac.th>
2022-12-16 12:15:30 +0000

    update file

--- 00-capstone-project/main.py
+++ 00-capstone-project/main.py
@@ -19,19 +19,55 @@ s3 = boto3.resource(
     aws_session_token=aws_session_token
 )
 s3.meta.client.upload_file(
-    "transaction_data.csv",
+    "superstore.csv",
     "lillythamolwan-titanic",
-    "transaction_data.csv",
+    "superstore.csv",
 )
 
-s3.meta.client.upload_file(
-    "product.csv",
-    "lillythamolwan-titanic",
-    "product.csv",
-)
+import psycopg2
 
-s3.meta.client.upload_file(
-    "hh_demographic.csv",
-    "lillythamolwan-titanic",
-    "hh_demographic.csv",
-)
+
+def main():
+    host = ""
+    dbname = "dev"
+    user = "awsuser"
+    password = ""
+    port = "5439"
+    conn_str = f"host={host} dbname={dbname} user={user} password={password} port={port}"
+    conn = psycopg2.connect(conn_str)
+    cur = conn.cursor()
+    
+    # Drop table if it exists
+    drop_table_query = "DROP TABLE IF EXISTS employees"
+    cur.execute(drop_table_query)
+    conn.commit()
+
+    # Create table
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS employees (
+        name text,
+        salary int
+    )
+    """
+    cur.execute(create_table_query)
+    conn.commit()
+
+    # Copy data from S3 to the table we created above
+    copy_table_query = """
+    COPY employees FROM 's3://zkan-swu-labs/employees.csv'
+    ACCESS_KEY_ID ''
+    SECRET_ACCESS_KEY ''
+    SESSION_TOKEN ''
+    CSV
+    IGNOREHEADER 1
+    REGION 'us-east-1'
+    """
+    cur.execute(copy_table_query)
+    conn.commit()
+
+    conn.close()
+
+
+if __name__ == "__main__":
+    main()
+
